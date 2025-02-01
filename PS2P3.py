a3f052546e41bfd5e349dcc391a1217f70999aa2
Miroslavsto <hi97319@mail.harpercollege.edu>
2025-01-31 19:34:21 -0600

    Delete PS2P3.py

--- PS2P3.py
+++
@@ -1,5 +0,0 @@
-def split_amount():
-    total_amount = float(input("Enter the total amount received: "))
-    amount_per_person = total_amount / 3
-    print(f"Each person will receive: ${amount_per_person:.2f}")
-split_amount()
